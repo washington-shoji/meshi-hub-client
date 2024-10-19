4f4cde457528dc5162ae7f3fd1bb9bc2aefa84c9
washington-shoji <shoji.washington@gmail.com>
2024-10-19 20:18:59 +1100

    - updated app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,42 @@
-import Image from "next/image";
 import FoodCard from "./components/FoodCard";
-import { console } from "inspector";
-
-async function getData() {
-  const res = await fetch("https://dummyjson.com/posts");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  console.log("res", res);
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import { getFoodPlaceAllInfo } from "./services/food_places_service";
 
 export default async function Home() {
-  const cardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const data = await getFoodPlaceAllInfo();
 
-  const data: any = await getData();
-
-  return (
-    <div>
+  if (data && data instanceof Error) {
+    return (
+      <main className="min-h-[80vh] flex flex-col justify-center items-center gap-4">
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            <span className="font-medium">An error Occurred!</span>{" "}
+            {data?.message}
+          </div>
+        </div>
+      </main>
+    );
+  } else if (data) {
+    return (
       <main className="flex flex-col gap-4">
         <div className="grid grid-cols-[repeat(auto-fit,_minmax(450px,_1fr))] justify-center gap-16 p-16">
-          {cardArray.map((c, i) => {
-            return <FoodCard key={i} />;
+          {data?.map((d, i) => {
+            return <FoodCard key={i} data={d} />;
           })}
         </div>
       </main>
-    </div>
-  );
+    );
+  }
 }
