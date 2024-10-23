452fbc57cbb589098c70c19bcb3da9135378d770
washington-shoji <shoji.washington@gmail.com>
2024-10-24 08:18:13 +1100

    - added food place admin page

---
+++ src/app/(dashboard)/food-place-admin/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import CreateFoodPlaceForm from "@/app/components/CreateFoodPlaceForm";
+import FoodPlaceAdminTable from "@/app/components/FoodPlaceAdminTable";
+import { useState } from "react";
+
+export default function CreateFoodPlace() {
+  const [createForm, setCreateForm] = useState<boolean>(false);
+  function togleCreateForm(): void {
+    setCreateForm(!createForm);
+  }
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4">
+      {!createForm && <FoodPlaceAdminTable togleCreateForm={togleCreateForm} />}
+
+      {createForm && <CreateFoodPlaceForm togleCreateForm={togleCreateForm} />}
+    </section>
+  );
+}
